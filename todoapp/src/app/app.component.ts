626555e6a958e34c14776cf587f8a8cfdee3e698
unknown <timothy.partaliano@bithealth.co.id>
2023-12-07 17:45:34 +0700

    add get function

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp';
+  readonly APIUrl="http://localhost:5038/api/todoapp/";
+
+  constructor(private http:HttpClient){
+  }
+  notes:any=[];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+      this.notes=data
+    })
+  }
+
+  ngOnInit(){
+    this.refreshNotes();
+  }
 }
